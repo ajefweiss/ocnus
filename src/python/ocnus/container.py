250da103a7bb041fd3e55642bc0f14f7037bb835
Andreas J. Weiss <ajefweiss@gmail.com>
2024-07-12 09:34:34 -0400

    container class

--- src/python/ocnus/container.py
+++ src/python/ocnus/container.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from typing import Mapping
+from typing import Iterable, Mapping
 
 import numpy as np
 import pyjson5 as json
+import scipy as sp  # noqa: F401
 
 # from .rotqs import generate_quaternions
-from .util import rng_truncanted
+
 
 lg = logging.getLogger(__name__)
 
 
 class ModelContainer(object):
-    dtype: np.float32
+    """Generic model container to handle a variety of different magnetic flux rope models.
 
-    model_params: dict
-    model_params_array: np.ndarray
+    Implcitly assumes that any model lives within its own Cartesian coordinate system,
+    with a propagation direction along the x-axis. Any model must implement a rot_z/y/x parameter
+    (which are the first three given parameters) with which one can rotate the geometry back
+    into a heliospheric coordinate system.
+    """
+
+    dtype: type
+
+    parameters: dict
+
+    param_larr: Iterable[np.ndarray]  # list of arrays holding the model parameters
+    state_larr: Iterable[np.ndarray]  # list of arrays holding the current model state
 
     quaterions: np.ndarray
 
@@ -41,28 +52,43 @@ class ModelContainer(object):
 
     def generate_model_params(
         self,
-        params: str | Mapping,
+        parameters: str | Mapping,
         max_iterations: int = 50,
         overwrite_array: bool = False,
     ) -> None:
-        if isinstance(params, str):
-            # load from file
-            self.model_params = json.decode_io(open(params, "rb", encoding="utf-8"))
-        elif not hasattr(params, "__getitem__"):
-            raise TypeError("params must be str or dict, not %s", type(params))
-        else:
-            self.model_params = params
+        """Generate initial model parameters.
 
-        if hasattr(self, "model_params_array") and not overwrite_array:
-            raise lg.warning(
-                "model_params_array already exists, set overwrite_array to turn this warning off"
+        Args:
+            parameters (str | Mapping): path to json file or equivalent dict object.
+            max_iterations (int, optional): maximum number of iterations allowed for the truncated random number
+                                            generator. Defaults to 50.
+            overwrite_array (bool, optional): allow overwriting of existing model parameters (warning only).
+                                              Defaults to False.
+
+        Raises:
+            TypeError: invalid parameters argument
+            ValueError: invalid parameters range (common mistake is max_value < min_value)
+        """
+        from .util import rng_truncanted
+
+        if isinstance(parameters, str):
+            # load from file
+            self.parameters = json.decode_io(open(parameters, "rb", encoding="utf-8"))
+        elif not hasattr(parameters, "__getitem__"):
+            raise TypeError("parameters must be str or dict, not %s", type(parameters))
+        else:
+            self.parameters = parameters
+
+        if hasattr(self, "param_larr") and not overwrite_array:
+            lg.warning(
+                "param_larr already exists, set overwrite_array to turn this warning off"
             )
 
-        self.model_params_array = np.empty(
-            (self.ensemble_size, len(self.model_params)), dtype=self.dtype
+        self.param_larr = np.empty(
+            (self.ensemble_size, len(self.parameters)), dtype=self.dtype
         )
 
-        for key, param in self.model_params.items():
+        for key, param in self.parameters.items():
             index = param["param_index"]
             distribution = param["distribution"]
 
@@ -79,9 +105,9 @@ class ModelContainer(object):
                         param.get("label", index),
                     )
 
-                self.model_params_array[:, index] = param["def_value"]
+                self.param_larr[:, index] = param["def_value"]
             elif distribution == "uniform":
-                self.model_params_array[:, index] = (
+                self.param_larr[:, index] = (
                     np.random.rand(self.ensemble_size)
                     * (param["max_value"] - param["min_value"])
                     + param["min_value"]
@@ -91,6 +117,7 @@ class ModelContainer(object):
                 try:
                     identifiers = distribution.split(".")
 
+                    # only allow for numpy or scipy functions
                     assert identifiers[0] in ["np", "sp"]
 
                     func = globals()[identifiers[0]]
@@ -98,28 +125,29 @@ class ModelContainer(object):
                     for name in identifiers[1:]:
                         func = getattr(func, name)
 
-                    self.model_params_array[:, index] = rng_truncanted(
+                    self.param_larr[:, index] = rng_truncanted(
                         func,
                         param["max_value"],
                         param["min_value"],
                         self.ensemble_size,
                         max_iterations ** param["kwargs"],
                     )
-                except Exception as e:
+                except RuntimeError as e:
                     lg.error("failed to use distribution %s (%s)", distribution, e)
                     raise
 
+        # generate quaternions for new model parameters
         self._update_quaternions()
 
-    def set_model_params(self, model_params_array: np.ndarray) -> None:
-        if self.ensemble_size != len(model_params_array):
+    def set_model_params(self, param_larr: np.ndarray) -> None:
+        if self.ensemble_size != len(param_larr):
             raise ValueError(
                 "array size does not match ensemble size (%i!=%i)",
                 self.ensemble_size,
-                len(model_params_array),
+                len(param_larr),
             )
 
-        self.model_params_array = model_params_array
+        self.param_larr = param_larr
         self._update_quaternions()
 
     def _update_quaternions(self) -> None:
@@ -127,7 +155,7 @@ class ModelContainer(object):
             self.quaterions = np.empty((self.ensemble_size, 4), dtype=self.dtype)
 
         # generate_quaternions(
-        #     self.model_params_array, self.quaterions, indices=[0, 1, 2]
+        #     self.param_larr, self.quaterions, indices=[0, 1, 2]
         # )
 
         # _rust.generate_quaternions()
---
+++ src/python/ocnus/models/ccm.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from ..container import ModelContainer
+
+
+class CCM(ModelContainer):
+    """Generic class for any circular cylindrical flux rope models."""
+
+    pass
